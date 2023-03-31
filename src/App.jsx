419e091b5d89f6abdd8ceff887e18812e8467e25
HighError <46526245+HighError@users.noreply.github.com>
2023-03-31 15:48:03 +0300

    feat(index.html): add link to Google Fonts and import Raleway font feat(index.html): change title to Weather App feat(public): add images for rain, snow, and sun feat(public): remove vite.svg icon file
    feat(App.jsx): add weather UI to App component
    feat(index.css): add background image and styles for weather UI
    feat(tailwind.config.js): add custom height value for full device height

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="rain absolute top-0 left-0 right-0 bottom-0" />
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center gap-3">
+        <div className="text-[24px] font-medium">Ukraine, Ternopil</div>
+        <div className="text-[96px] font-bold">13°C</div>
+        <div className="flex flex-col items-center">
+          <div className="">31.03.2023</div>
+          <div className="">Сонячно</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        fulldvh: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
